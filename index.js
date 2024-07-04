9a777c4515c72eb20aa88027bc3fbe4d5f3d080e
Emanuelly16 <165863187+Emanuelly16@users.noreply.github.com>
2024-07-04 00:41:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,121 @@
+/* 1.
+let cotacao = Number(prompt("Digite a cotação do dólar"));
+let valorreal = Number(prompt("Qual o seu valor em reais?"));
+let conversao = valorreal / cotacao;
+alert(conversao);*/
+
+/*
+let usuario = Number(prompt("Digite um número"));
+if (usuario % 2 == 0){
+console.log(usuario,"é par");
+}else{
+console.log(usuario,"é ímpar");
+}*/
+
+/* 3.
+let num1 = 20
+let num2 = 10
+if (num1<num2){
+console.log("O número 2 [e o maior");
+}else {
+ console.log("O número 1 é maior ");
+}*/ 
+
+/*4. 
+let prova1 = Number(prompt("Digite a 1º nota"));
+let prova2 = Number(prompt("Digite a 2º nota"));
+let soma = prova1 + prova2;
+if (soma < 6){
+console.log("Aluno reprovado");
+}else{
+    console.log("Aluno aprovado");
+}*/
+
+/* 5. 
+let Ingresso_VIP_sem_pipoca = 80;
+let Ingresso_VIP_com_pipoca =  110;
+let Ingresso_standard_sem_pipoca = 40;
+let Ingresso_standard_com_pipoca =  70;
+let pipoca = Number(prompt("Qual ingresso você quer?"));
+let pipoca_com = Number(prompt("Deseja pipoca?"));
+if (pipoca == 1 && pipoca_com == 1){
+    console.log("O valor do ingresso VIP com pipoca é de R$ 110");
+}else if (pipoca == 1 && pipoca_com == 0){
+    console.log("O valor do ingresso VIP sem pipoca é de R$ 80");
+    }else if (pipoca == 2 && pipoca_com == 1){
+        console.log("O valor do ingresso standard com pipoca é de R$ 70");
+        }else if (pipoca == 2 && pipoca_com == 0){
+            console.log("O valor do ingresso standard sem pipoca é de R$ 40");
+            }else{
+                console.log("Valor inválido");} */
+
+/*
+6. NÃO CONSEGUI FAZER
+let usuario = Number(prompt("Escolha uma opção"));
+switch(usuario){
+    case 1:"Você quer escolher uma música?";
+    console.log("Você escolheu a opção 1");
+        break;
+        case 2:"Você quer escolher um filme?";
+        console.log("Você escolheu a opção 2");
+        break;
+        case 3:"Você quer escolher um livro?";
+        console.log("Você escolheu a opção 3");
+        break;
+        default:"Valor inválido";
+        break;
+if (usuario == 1){
+    console.log("O catalogo de músicas esta carregando, aguarde");
+    console.log("Você selecionou uma música");
+    console.log("encerrando");}
+else if (usuario == 2)
+    console.log("O catalogo de filmes esta carregando, aguarde");
+    console.log("Você selecionou um filme");
+    console.log("encerrando");
+else if (usuario == 3)
+    console.log("O catalogo de livros esta carregando, aguarde");
+    console.log("Você selecionou um livro");
+    console.log("encerrando");}*/
+
+/* 7.       NÃO CONSEGUI FAZER
+const mes = 28
+const horasemanal = 40
+let horas_mes = Number(prompt("Digite quantidade de horas trabalhadas por"));
+let salario_hora = Number(prompt("Digite quanto vale sua hora de trabalho"))
+let salario = horas_mes * salario_hora;
+if (horas_mes===160){
+    console.log(salario,"normal")
+}else if {
+    (horas_mes > 160)
+     mult == 160*0,5
+    salario = mult
+    console.log(salario );}*/
+
+/*8.
+let usuario = Number(prompt("Digite um número"));
+let usuario1 = Number(prompt("Digite outro número"));
+let calculo = prompt("Digite o tipo de calculo desejado")
+let soma = usuario + usuario1;
+let subtracao = usuario - usuario1;
+let multiplicacao = usuario * usuario1;
+let divisao = usuario / usuario1;
+if (calculo == "soma"){
+    console.log(soma)
+    }else if (calculo == "subtracao"){
+        console.log(subtracao)
+        }else if (calculo == "multiplicacao"){
+            console.log(multiplicacao)
+            }else if (calculo == "divisao"){
+                console.log(divisao)
+                }else {
+                    console.log("Valor inválido")
+*/
+
+ 
+    
+
+
+
+
+
+
